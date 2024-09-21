4531436a963f69ecf3e9ee83ec73cf1cc92a2189
Olehmuz <olehmuz87@gmail.com>
2024-09-22 01:08:22 +0300

    Fix unit tests

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,10 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello World! Change to trigger CI/CD"', () => {
+      expect(appController.getHello()).toBe(
+        'Hello World! Change to trigger CI/CD',
+      );
     });
   });
 });
